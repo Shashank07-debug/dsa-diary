8bf93cbce00d3165983344c3c3d69ee82c4b0a14
shashank007 <shashankmn066@gmail.com>
2025-08-25 08:19:29 +0530

    "SecondLargest Optimal"

---
+++ DSA/com/arrayProblems/CheckSortesAndRotated.java
@@ -0,0 +1,33 @@
+package com.arrayProblems;
+
+public class CheckSortesAndRotated {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CheckSortesAndRotated sol = new CheckSortesAndRotated();
+        int[] arr1 = {3, 4, 5, 1, 2};
+        int[] arr2 = {1, 2, 3, 4, 5};
+        int[] arr3 = {2, 1, 3, 4, 5};
+        int[] arr4 = {5, 1, 2, 3, 4};
+        int[] arr5 = {1, 3, 2, 4, 5};
+
+        System.out.println("Test case 1: " + sol.check(arr1)); // true
+        System.out.println("Test case 2: " + sol.check(arr2)); // true
+        System.out.println("Test case 3: " + sol.check(arr3)); // false
+        System.out.println("Test case 4: " + sol.check(arr4)); // true
+        System.out.println("Test case 5: " + sol.check(arr5)); // false
+    }
+}
+
