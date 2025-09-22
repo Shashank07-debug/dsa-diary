b129b68b2270559ef8345091971bbf7a4642fbc0
shashank007 <shashankmn066@gmail.com>
2025-09-22 09:22:41 +0530

    "Minimum divisor with the given threshold"

--- DSA/com/BinarySearch/FindMinimumDivisor.java
+++ DSA/com/BinarySearch/FindMinimumDivisor.java
@@ -43,7 +43,6 @@ public class FindMinimumDivisor {
         }
         return (sum <= threshold);
     }
-    // main method
     public static void main(String[] args) {
         FindMinimumDivisor solver = new FindMinimumDivisor();
         int[] nums = {1, 2, 5, 9};
